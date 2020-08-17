2c00f8ae35174f4946202b5fe565948b8175d4c4
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-08-16 21:45:28 -0300

    Query backend data

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -3,6 +3,10 @@ import { TimeSplit } from "./typings/global";
 import { tick } from "./utils/time";
 import { useCssHandles } from "vtex.css-handles";
 
+import { useQuery } from "react-apollo";
+import useProduct from "vtex.product-context/useProduct";
+import productReleaseDate from "./graphql/productReleaseDate.graphql";
+
 const DEFAULT_TARGET_DATE = new Date("2020-06-25").toISOString();
 const CSS_HANDLES = ["countdown"];
 
@@ -19,9 +23,41 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     seconds: "00",
   });
 
+  const {
+    product: { linkText },
+  } = useProduct();
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText,
+    },
+    ssr: false,
+  });
+
   const handles = useCssHandles(CSS_HANDLES);
 
-  tick(targetDate, setTime);
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime);
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    );
+  }
+  if (!product) {
+    return (
+      <div>
+        <span>Não há contexto de produto</span>
+      </div>
+    );
+  }
 
   return (
     <div className={`${handles.countdown} db tc`}>
